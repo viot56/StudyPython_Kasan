229dd9dc3e6150bc3f95b776372134555f4ddf11
viot56 <chltlagud12@naver.com>
2022-05-23 17:11:51 +0900

    220523 깃헙 최초 커밋

---
+++ day1/test.py
@@ -0,0 +1,30 @@
+
+print('Hello, Python!')
+
+a = 'Hello, Python'
+print(a)
+
+a = 10
+print(a)
+
+a = False
+print(a)
+
+#주석, 변수명 지정, 영문자로 시작하고 _는 허용
+#대소문자 구분
+var1 = 17
+Var1 = 19
+바20 = 20
+print(Var1)
+print(var1)
+#4var = 4
+#5_var = 5
+#remaininOfBankAccount #은행계좌 잔고 
+#변수명 지정은 의미있는 것으로
+
+
+print(var1 == Var1)
+
+
+#자료형
+#자료 = data, information, acknowledge => 의미있는 데이터 -> information technology -> ackowledge
